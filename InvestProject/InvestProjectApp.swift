405c7f1f6176a3e173b505840c980f3e01136e49
Nauan_M1_PRO <Nauan.Akylbayev@eubank.kz>
2023-11-25 20:09:38 +0300

    Added all files in repository

---
+++ InvestProject/InvestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvestProjectApp.swift
+//  InvestProject
+//
+//  Created by Admin on 25.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct InvestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
